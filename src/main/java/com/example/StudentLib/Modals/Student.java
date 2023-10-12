4db313d26bc997c4f12a733bed008a4ef5030f1c
kaushaljoshi977 <kaushaljoshi977@gmail.com>
2023-10-12 14:44:21 +0530

    sec comm

--- src/main/java/com/example/StudentLib/Modals/Student.java
+++ src/main/java/com/example/StudentLib/Modals/Student.java
@@ -14,8 +14,12 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "contact",unique = true)
     private String contact;
 }
